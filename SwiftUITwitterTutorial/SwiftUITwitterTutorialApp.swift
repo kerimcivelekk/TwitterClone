cfcd0a979ff7366a274d44301f1c0749f30f9e30
Kerim <targetbad@hotmail.com>
2022-07-14 16:47:43 +0300

    Initial Commit

---
+++ SwiftUITwitterTutorial/SwiftUITwitterTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUITwitterTutorialApp.swift
+//  SwiftUITwitterTutorial
+//
+//  Created by Kerim Civelek on 14.07.2022.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUITwitterTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
